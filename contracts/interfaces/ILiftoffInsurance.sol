ae0796ae76a89059f98cc0307dd5d918af947eef
Kazune Takeda <58710601+kazunetakeda25@users.noreply.github.com>
2020-12-14 13:31:09 -0800

    Add liftoff insurance request registration to LiftoffEngine

--- contracts/interfaces/ILiftoffInsurance.sol
+++ contracts/interfaces/ILiftoffInsurance.sol
@@ -1,7 +1,8 @@
 pragma solidity 0.5.16;
 
 interface ILiftoffInsurance {
-  function acceptDeposit(address _token) external;
+  function requestRegistration(uint _tokenSaleId, uint xEthBuy) external;
+  function completeRegistration(uint _tokenSaleId) external;
   function refund(address _token, uint _amount) external;
   function withdraw(address _token, uint _amount, address _for) external;
 }
