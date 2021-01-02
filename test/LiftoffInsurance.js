0d4969df13ad10f2af16ec1bf9fb88b8755301de
Kazune Takeda <58710601+kazunetakeda25@users.noreply.github.com>
2021-01-01 16:22:43 -0800

    Add LiftoffInsurance tests for Stateles and State: PreRegistration except getTotalXethClaimable

--- test/LiftoffInsurance.js
+++ test/LiftoffInsurance.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const { solidity } = require("ethereum-waffle");
 const { expect } = chai;
-const { ether, time } = require("@openzeppelin/test-helpers");
+const { ether, time, BN } = require("@openzeppelin/test-helpers");
 const { UniswapDeployAsync } = require("../tools/UniswapDeployAsync");
 const { XLockDeployAsync } = require("../tools/XLockDeployAsync");
 const loadJsonFile = require('load-json-file');
@@ -50,7 +50,7 @@ describe('LiftoffInsurance', function () {
       settings.projectDevBP,
       settings.mainFeeBP,
       settings.lidPoolBP
-    )
+    );
 
     await liftoffSettings.setAllAddresses(
       liftoffInsurance.address,
@@ -61,50 +61,217 @@ describe('LiftoffInsurance', function () {
       uniswapV2Router02.address,
       lidTreasury.address,
       lidPoolManager.address
-    )
-  })
+    );
+  });
 
   describe("Stateless", function() {
+    describe("isInsuranceExhausted", function() {
+      let currentTime, startTime, insurancePeriod, xEthValue, baseXEth, redeemedXEth, isUnwound;
+      before(async function () {
+        startTime = await time.latest();
+        insurancePeriod = new BN(settings.insurancePeriod);
+        currentTime = startTime.add(insurancePeriod).add(time.duration.days(1));
+        xEthValue = ether("10");
+        baseXEth = ether("100");
+        redeemedXEth = ether("100");
+        isUnwound = false;
+      });
+      it("Should be true if not isUnwound, currentTime > startTime+insurancePeriod, and baseXEth < redeemedXEth + xEthValue", async function() {
+        const isExhausted = await liftoffInsurance.isInsuranceExhausted(
+          currentTime.toString(),
+          startTime.toString(),
+          insurancePeriod.toString(),
+          xEthValue.toString(),
+          baseXEth.toString(),
+          redeemedXEth.toString(),
+          isUnwound
+        );
+        expect(isExhausted).to.be.true;
+      });
+      it("Should be false if isUnwound", async function() {
+        const isExhausted = await liftoffInsurance.isInsuranceExhausted(
+          currentTime.toString(),
+          startTime.toString(),
+          insurancePeriod.toString(),
+          xEthValue.toString(),
+          baseXEth.toString(),
+          redeemedXEth.toString(),
+          true
+        );
+        expect(isExhausted).to.be.false;
+      });
+      it("Should be false if currentTime <= startTime+insurancePeriod", async function() {
+        const isExhausted = await liftoffInsurance.isInsuranceExhausted(
+          startTime.add(insurancePeriod).toString(),
+          startTime.toString(),
+          insurancePeriod.toString(),
+          xEthValue.toString(),
+          baseXEth.toString(),
+          redeemedXEth.toString(),
+          isUnwound
+        );
+        expect(isExhausted).to.be.false;
+      });
+      it("Should be false if baseXEth > redeemedXEth + xEthValue", async function() {
+        const isExhausted = await liftoffInsurance.isInsuranceExhausted(
+          currentTime.toString(),
+          startTime.toString(),
+          insurancePeriod.toString(),
+          xEthValue.toString(),
+          baseXEth.toString(),
+          ether("0").toString(),
+          isUnwound
+        );
+        expect(isExhausted).to.be.false;
+      });
+    });
+    describe("canCreateInsurance", function() {
+      let insuranceIsInitialized, tokenIsRegistered;
+      before(async function() {
+        insuranceIsInitialized = false;
+        tokenIsRegistered = true;
+      });
+      it("Should be true if not insuranceIsInitialized and is tokenRegistered", async function() {
+        const canCreateInsurance = await liftoffInsurance.canCreateInsurance(
+          insuranceIsInitialized,
+          tokenIsRegistered
+        );
+        expect(canCreateInsurance).to.be.true;
+      });
+      it("Should be false if insuranceIsInitialized", async function() {
+        const canCreateInsurance = await liftoffInsurance.canCreateInsurance(
+          true,
+          tokenIsRegistered
+        );
+        expect(canCreateInsurance).to.be.false;
+      });
+      it("Should be false if not tokenRegistered", async function() {
+        const canCreateInsurance = await liftoffInsurance.canCreateInsurance(
+          insuranceIsInitialized,
+          false
+        );
+        expect(canCreateInsurance).to.be.false;
+      });
+    });
+    describe("getRedeemValue", function() {
+      it("Should be equal to amount divided by tokens/eth when price is above one", async function() {
+        const amount = ether("3193.12321");
+        const tokensPerEthWad = ether("3241.2313");
+        const redeemValue = await liftoffInsurance.getRedeemValue(
+          amount.toString(),
+          tokensPerEthWad.toString()
+        );
+        const expectedAmount = amount.mul(ether("1")).div(tokensPerEthWad).toString();
+        expect(redeemValue).to.be.bignumber.above(ether("0.8").toString());
+        expect(redeemValue).to.be.bignumber.below(ether("1").toString());
+        expect(redeemValue).to.be.bignumber.equal(expectedAmount);
+      });
+      it("Should be equal to amount divided by tokens/eth when price is below one", async function() {
+        const amount = ether("0.12321");
+        const tokensPerEthWad = ether("0.001313121187");
+        const redeemValue = await liftoffInsurance.getRedeemValue(
+          amount.toString(),
+          tokensPerEthWad.toString()
+        );
+        const expectedAmount = amount.mul(ether("1")).div(tokensPerEthWad).toString();
+        expect(redeemValue).to.be.bignumber.above(ether("80").toString());
+        expect(redeemValue).to.be.bignumber.below(ether("100").toString());
+        expect(redeemValue).to.be.bignumber.equal(expectedAmount);
+      });
+    });
+    describe("getTotalTokenClaimable", function() {
+      it("Should be 0 when cycles are 0", async function() {
+        const totalClaimable = await liftoffInsurance.getTotalTokenClaimable(
+          ether("12000").toString(),
+          0,
+          0
+        );
+        expect(totalClaimable).to.be.bignumber.equal(0);
+      });
+      it("Should be base*cycles/10-claimed when cycles less than 10", async function() {
+        const base = ether("12000");
+        const claimed = ether("3600");
+        const cycles = new BN("6");
+        const totalClaimable = await liftoffInsurance.getTotalTokenClaimable(
+          base.toString(),
+          cycles.toString(),
+          claimed.toString()
+        );
+        expect(totalClaimable).to.be.bignumber.equal(base.mul(cycles).div(new BN("10")).sub(claimed).toString());
+      });
+      it("Should be base-claimed when cycles greater than 10", async function() {
+        const base = ether("12000");
+        const claimed = ether("3600");
+        const cycles = new BN("11");
+        const totalClaimable = await liftoffInsurance.getTotalTokenClaimable(
+          base.toString(),
+          cycles.toString(),
+          claimed.toString()
+        );
+        expect(totalClaimable).to.be.bignumber.equal(base.sub(claimed).toString());
+      });
+    });
+    describe("getTotalXethClaimable", function() {
+      let totalIgnited, redeemedXEth, claimedXeth;
+      before(async function() {
+        totalIgnited = ether("3500");
+        redeemedXEth = ether("1200");
+        claimedXeth = ether("230");
+        cycles = new BN("3");
+      })
+      it("Should be 0 when cycles are 0", async function() {
+        const totalClaimable = await liftoffInsurance.getTotalXethClaimable(
+          totalIgnited.toString(),
+          redeemedXEth.toString(),
+          claimedXeth.toString(),
+          0
+        );
+        expect(totalClaimable).to.be.bignumber.equal(0);
+      });
+      //TODO: Fix LiftoffInsurance.getTotalXethClaimable to not use totalIgnited
+      //Instead, should use totalIgnited - total buy - base fees 
+      /*it("Should be (total - redeemed * cycles / 10 when cycles are 3", async function() {
+        const totalClaimable = await liftoffInsurance.getTotalXethClaimable(
+          totalIgnited.toString(),
+          redeemedXEth.toString(),
+          claimedXeth.toString(),
+          0
+        );
+        const expectedValue = totalIgnited.sub(redeemedXEth). 
+        expect(totalClaimable).to.be.bignumber.equal(expectedValue.toString());
+      });*/
+    });
+  });
+  describe("State: PreRegisteration", function() {
     describe("register", function() {
       it("should revert if sender is not liftoffEngine", async function() {
         await expect(
           liftoffInsurance.register(0)
         ).to.be.revertedWith("Sender must be Liftoff Engine")
-      })
-    })
+      });
+    });
     describe("redeem", function() {
       it("should revert if insurance is not initialized for tokenSaleId", async function() {
         await expect(
           liftoffInsurance.redeem(0,0)
         ).to.be.revertedWith("Insurance not initialized")
-      })
+      });
     });
     describe("claim", function() {
       it("should revert if insurance is not initialized for tokenSaleId", async function() {
         await expect(
           liftoffInsurance.claim(0)
         ).to.be.revertedWith("Insurance not initialized")
-      })
+      });
     });
-    /*describe("createInsurance", function() {
-
+    describe("createInsurance", function() {
+      it("should revert if insurance is not initialized for tokenSaleId", async function() {
+        await expect(
+          liftoffInsurance.createInsurance(0)
+        ).to.be.revertedWith("Cannot create insurance")
+      });
     });
-    describe("isInsuranceExhausted", function() {
-
-    });
-    describe("canCreateInsurance", function() {
-
-    });
-    describe("getRedeemValue", function() {
-
-    });
-    describe("getTotalTokenClaimable", function() {
-
-    });
-    describe("getTotalXethClaimable", function() {
-
-    });*/
-  })
+  });
   describe("State: Insurance Registered", function() {
     /*describe("register", function() {
 
@@ -117,23 +284,8 @@ describe('LiftoffInsurance', function () {
     });
     describe("createInsurance", function() {
 
-    });
-    describe("isInsuranceExhausted", function() {
-
-    });
-    describe("canCreateInsurance", function() {
-
-    });
-    describe("getRedeemValue", function() {
-
-    });
-    describe("getTotalTokenClaimable", function() {
-
-    });
-    describe("getTotalXethClaimable", function() {
-
     });*/
-  })
+  });
   describe("State: Insurance Initialized", function() {
     /*describe("register", function() {
 
@@ -146,23 +298,8 @@ describe('LiftoffInsurance', function () {
     });
     describe("createInsurance", function() {
 
-    });
-    describe("isInsuranceExhausted", function() {
-
-    });
-    describe("canCreateInsurance", function() {
-
-    });
-    describe("getRedeemValue", function() {
-
-    });
-    describe("getTotalTokenClaimable", function() {
-
-    });
-    describe("getTotalXethClaimable", function() {
-
     });*/
-  })
+  });
   describe("State: Insurance Cycle 1", function() {
     /*describe("register", function() {
 
@@ -175,23 +312,8 @@ describe('LiftoffInsurance', function () {
     });
     describe("createInsurance", function() {
 
-    });
-    describe("isInsuranceExhausted", function() {
-
-    });
-    describe("canCreateInsurance", function() {
-
-    });
-    describe("getRedeemValue", function() {
-
-    });
-    describe("getTotalTokenClaimable", function() {
-
-    });
-    describe("getTotalXethClaimable", function() {
-
     });*/
-  })
+  });
   describe("State: Insurance Cycle 5", function() {
     /*describe("register", function() {
 
@@ -204,23 +326,8 @@ describe('LiftoffInsurance', function () {
     });
     describe("createInsurance", function() {
 
-    });
-    describe("isInsuranceExhausted", function() {
-
-    });
-    describe("canCreateInsurance", function() {
-
-    });
-    describe("getRedeemValue", function() {
-
-    });
-    describe("getTotalTokenClaimable", function() {
-
-    });
-    describe("getTotalXethClaimable", function() {
-
     });*/
-  })
+  });
   describe("State: Insurance Cycle 11", function() {
     /*describe("register", function() {
 
@@ -233,23 +340,8 @@ describe('LiftoffInsurance', function () {
     });
     describe("createInsurance", function() {
 
-    });
-    describe("isInsuranceExhausted", function() {
-
-    });
-    describe("canCreateInsurance", function() {
-
-    });
-    describe("getRedeemValue", function() {
-
-    });
-    describe("getTotalTokenClaimable", function() {
-
-    });
-    describe("getTotalXethClaimable", function() {
-
     });*/
-  })
+  });
   describe("State: Insurance Exhausted", function() {
     /*describe("register", function() {
 
@@ -262,21 +354,6 @@ describe('LiftoffInsurance', function () {
     });
     describe("createInsurance", function() {
 
-    });
-    describe("isInsuranceExhausted", function() {
-
-    });
-    describe("canCreateInsurance", function() {
-
-    });
-    describe("getRedeemValue", function() {
-
-    });
-    describe("getTotalTokenClaimable", function() {
-
-    });
-    describe("getTotalXethClaimable", function() {
-
     });*/
-  })
+  });
 });
