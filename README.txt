27c2eee759e370573b300ac4a505b0723e131523
carlsbad <carlsbad_sunshine@protonmail.com>
2021-01-16 21:56:07 -0800

    Deploy ropsten partnerships upgrade

--- README.txt
+++ README.txt
@@ -1,11 +1,11 @@
-Deployments v 0.1.2
+Deployments v 0.1.3
 
 Ropsten
 LiftoffSettings:     0x71A442F174EA408f762624981c07Ca3F800Aa72E
 LiftoffEngine:       0xF4CCd4483b393A0526FF939CF15E89eBd3958f2B
 LiftoffInsurance:    0x4f250e2236457259BDac9f651f62e972Ce502Caa
 LiftoffRegistration: 0xadd1539fb19e03eE66fd365a6F6Cad68b49f981C
-LiftoffPartnerships: 
+LiftoffPartnerships: 0x58711cBF62585bcb907237D3CC047664e62BfD99
 
 Mainnet
 LiftoffSettings:     0xF54d9fC14A006763C83d4e12B1BB5dFB02eA668c
--- scripts/upgrade-1-partnerships.js
+++ scripts/upgrade-1-partnerships.js
@@ -1,7 +1,7 @@
 const { BigNumber } = require("ethers");
 const { ethers, upgrades } = require("hardhat");
 const loadJsonFile = require('load-json-file');
-const addresses = loadJsonFile.sync("./scripts/settings.json").networks.ropsten;
+const addresses = loadJsonFile.sync("./scripts/addresses.json").networks.ropsten;
 
 async function main() {
   //Silences "struct" warnings
@@ -14,17 +14,17 @@ async function main() {
   const LiftoffEngine = await ethers.getContractFactory("LiftoffEngine")
   const LiftoffPartnerships = await ethers.getContractFactory("LiftoffPartnerships")
 
-  console.log("Deploying liftoff partnerships...");
-  const liftoffPartnerships = await upgrades.deployProxy(LiftoffPartnerships, [addresses.liftoffSettings], {unsafeAllowCustomTypes: true});
+  console.log(`Deploying liftoff partnerships with liftofSettings @ ${addresses.LiftoffSettings}...`);
+  const liftoffPartnerships = await upgrades.deployProxy(LiftoffPartnerships, [addresses.LiftoffSettings], {unsafeAllowCustomTypes: true});
   await liftoffPartnerships.deployed();
   console.log("LiftoffPartnerships deployed to:", liftoffPartnerships.address);
 
   console.log("Upgrading liftoff engine...");
-  const liftoffEngine = await upgrades.upgradeProxy(addresses.liftoffEngine, LiftoffEngine);
+  const liftoffEngine = await upgrades.upgradeProxy(addresses.LiftoffEngine, LiftoffEngine, {unsafeAllowCustomTypes: true});
   console.log("Upgrading liftoff setings...");
-  const liftoffSettings = await upgrades.upgradeProxy(addresses.liftoffSettings, LiftoffSettings);
+  const liftoffSettings = await upgrades.upgradeProxy(addresses.LiftoffSettings, LiftoffSettings);
   console.log("Upgrading liftoff insurance...");
-  const liftoffInsurance = await upgrades.upgradeProxy(addresses.liftoffInsurance, LiftoffInsurance);
+  const liftoffInsurance = await upgrades.upgradeProxy(addresses.LiftoffInsurance, LiftoffInsurance, {unsafeAllowCustomTypes: true});
   console.log("Upgrades complete.");
 
   console.log("Calling setLiftoffPartnerships...");
