388099e7260696f435d946aa4de3b21ac974f231
devstar <dev_star11@outlook.com>
2021-01-22 13:00:01 +1000

    fix comments

--- scripts/upgrade-3-blacklistBuyAfterUnwind.js
+++ scripts/upgrade-3-blacklistBuyAfterUnwind.js
@@ -13,26 +13,10 @@ async function main() {
   const LiftoffEngine = await ethers.getContractFactory("LiftoffEngine");
   const LiftoffInsurance = await ethers.getContractFactory("LiftoffInsurance");
 
-  const liftoffSettings = await ethers.getContractAt("LiftoffSettings",addresses.LiftoffSettings);
-  const liftoffRegistration = await ethers.getContractAt("LiftoffRegistration",addresses.LiftoffRegistration);
-
   console.log("Upgrading liftoff engine...");
   const liftoffEngine = await upgrades.upgradeProxy(addresses.LiftoffEngine, LiftoffEngine, {unsafeAllowCustomTypes: true});
   console.log("Upgrading liftoff insurance...");
   const liftoffInsurance = await upgrades.upgradeProxy(addresses.LiftoffInsurance, LiftoffInsurance, {unsafeAllowCustomTypes: true});
-
-  console.log("liftoffSettings: Calling setLiftoffEngine...");
-  await liftoffSettings.setLiftoffEngine(
-    liftoffEngine.address
-  );
-  console.log("liftoffSettings: Calling setLiftoffInsurance...");
-  await liftoffSettings.setLiftoffInsurance(
-    liftoffInsurance.address
-  );
-  console.log("liftoffRegistration: Calling setLiftoffEngine...");
-  await liftoffRegistration.setLiftoffEngine(
-    liftoffEngine.address
-  );
 
   console.log("Script complete.");
 }
