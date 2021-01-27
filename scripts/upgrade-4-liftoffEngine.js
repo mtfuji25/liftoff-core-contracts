cfae8fe752b0050af60858789e31558cddb723a4
devstar <dev_star11@outlook.com>
2021-01-27 12:03:43 +1000

    add upgrade script

---
+++ scripts/upgrade-4-liftoffEngine.js
@@ -0,0 +1,26 @@
+// undoIgniteEth, claimRefundEth added into the liftoffEngine
+
+const { ethers, upgrades } = require("hardhat");
+const loadJsonFile = require('load-json-file');
+const addresses = loadJsonFile.sync("./scripts/addresses.json").networks.ropsten;
+
+async function main() {
+  //Silences "struct" warnings
+  //WARNING: do NOT add new properties, structs, mappings etc to these contracts in upgrades.
+  upgrades.silenceWarnings()
+
+  // We get the contract to deploy
+  const LiftoffEngine = await ethers.getContractFactory("LiftoffEngine");
+
+  console.log("Upgrading liftoff engine...");
+  const liftoffEngine = await upgrades.upgradeProxy(addresses.LiftoffEngine, LiftoffEngine, {unsafeAllowCustomTypes: true});
+
+  console.log("Script complete.");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  });
